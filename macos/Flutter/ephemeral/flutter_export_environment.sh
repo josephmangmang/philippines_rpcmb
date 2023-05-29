23a35b29dcf385be1db0c47364e298985a3e9d58
josephmangmang <joseph.deplacer@gmail.com>
2023-05-29 14:08:32 +0800

    Update homepage and repo url

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/josephmangmang/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/josephmangmang/Projects/rice-farm-inventory/philippines_rpcmb"
+export "FLUTTER_APPLICATION_PATH=/Users/josephmangmang/Projects/philippines_rpcmb"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=0.0.2"
+export "FLUTTER_BUILD_NUMBER=0.0.2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
